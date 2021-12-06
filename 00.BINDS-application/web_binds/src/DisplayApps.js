8149f3ec7d0b8fa3e47c5dafe0621ad90c87e9e3
Kunio Hirata <kuntaro@kundev.spring8.or.jp>
2021-12-06 15:40:30 +0900

    Now routinal works can be done by all components

---
+++ 00.BINDS-application/web_binds/src/DisplayApps.js
@@ -0,0 +1,58 @@
+// import axios from "axios";
+// import { useState } from "react";
+
+import { Button } from "@chakra-ui/button";
+import { Checkbox } from "@chakra-ui/checkbox";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Modal, ModalContent, ModalOverlay } from "@chakra-ui/modal";
+import { Table, TableCaption, Tbody, Tr, Th, Td } from "@chakra-ui/table";
+import { useCallback } from "react";
+
+export const DisplayApps = (props) => {
+    const { apps } = props;
+    console.log("DisplayApps");
+    /* chakra UI modal hooks*/
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    
+    const onClickTest = useCallback(()=>onOpen(),[]);
+
+    const selectFunc=(app)=>{
+        return(app.status==="支援中");
+    }
+
+    return (
+        <div>
+            <Table variant="striped" colorScheme="teal">
+                <TableCaption> BINDS application list </TableCaption>
+                <Tbody>
+                {apps.map((app) => (
+                    <Tr key={app._id}>
+                          <Checkbox size="md" colorScheme="green" defaultIsChecked />
+                        <Td>
+                            {app.id}
+                        </Td>
+                        <Td>
+                            {app.username}
+                        </Td>
+                        <Td>
+                            {app.status}
+                        </Td>
+                        <Td>
+                            {app.main_staff}
+                        </Td>
+                        <Td>
+                        <Button onClick={onClickTest} colorScheme="blue"> Modify </Button>
+                        </Td>
+                    </Tr>
+                ))}
+                </Tbody>
+            </Table>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <p> test </p>
+                </ModalContent>
+            </Modal>
+        </div>
+    );
+};
